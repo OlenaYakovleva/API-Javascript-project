56f2e543f41ab4a9da2845fd4f0e4408976c2a8b
Olena Yakovleva <alyona.jackovleva@gmail.com>
2024-02-27 20:16:00 +0100

    name is changed

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-
 let newText = "https://api.spaceflightnewsapi.net/v4/articles/";
- async function showArticles() { 
-  let response =  await fetch(newText);
+async function showArticles() {
+  let response = await fetch(newText);
   let data = await response.json();
   let results = data.results;
-  console.log (data); 
+  console.log(data);
 
-  for (let i = 0; i< results.length ; i++) {
-    let article = results [i];
+  for (let i = 0; i < results.length; i++) {
+    let article = results[i];
 
 
     let div = document.createElement('div');
-    let title = document.createElement ('h2');
+    let title = document.createElement('h2');
 
-title.textContent=article.title;
-div.appendChild(title);
+    title.textContent = article.title;
+    div.appendChild(title);
 
-let summary = document.createElement ('p');
-summary.textContent= article.summary;
-div.appendChild(summary);
+    let summary = document.createElement('p');
+    summary.textContent = article.summary;
+    div.appendChild(summary);
 
-let img = document.createElement ('img');
-img.src = article.image_url;
-img.width = 500;
-div.appendChild (img);
-new simpleParallax (img, {
-    orientation: 'right'
-});
+    let img = document.createElement('img');
+    img.src = article.image_url;
+    img.width = 500;
+    div.appendChild(img);
+    new simpleParallax(img, {
+      orientation: 'right'
+    });
 
-document.body.appendChild(div);
+    document.body.appendChild(div);
   }
 }
 
